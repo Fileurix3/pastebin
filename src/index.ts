faa1d3d332ef791827fb434c4aeef3445fd5b9a0
Fileurix3 <k96260831@gmail.com>
2025-01-30 13:03:54 +0200

    add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
+import { errorMiddleware } from "./middleware/error_middleware";
 import cookieParser from "cookie-parser";
 import minioClient from "./databases/minio";
 import postsRouter from "./services/posts/posts_router";
 import usersRouter from "./services/users/users_router";
 import redisClient from "./databases/redis";
 import authRouter from "./services/auth/auth_router";
+import swaggerUi from "swagger-ui-express";
 import sequelize from "./databases/db";
 import express from "express";
+import yaml from "yamljs";
+import path from "path";
 import "dotenv/config";
-import { errorMiddleware } from "./middleware/error_middleware";
 
 const app = express();
+const swaggerDocument = yaml.load(path.join(__dirname, "../swagger.yaml"));
 
 sequelize
   .authenticate()
@@ -28,6 +32,7 @@ minioClient
 
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/auth", authRouter);
 app.use("/post", postsRouter);
@@ -37,7 +42,10 @@ app.use(errorMiddleware);
 
 if (process.env.NODE_ENV != "test") {
   const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log(`Server was running on port: ${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`Server was running on port: ${PORT}`);
+    console.log(`Documentation in swagger: localhost:${PORT}/api-docs`);
+  });
 }
 
 export default app;
